e12bd8594ebdd6e294f1b689ca60c9cd277a4151
DavidDavtyan <davit.davtyan84@gmail.com>
2023-09-10 18:52:40 +0400

    change buscket component

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,23 +1,33 @@
 import { useContext } from "react"
 import Context from "./Context"
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import { BsFillTrashFill } from 'react-icons/bs'
 
 export default function Basket() {
     const value = useContext(Context);
     return (
         <div className="basket" style={{ clipPath: value.openbasket }}>
-
-            {value.buscetProduct.map(product =>
-                <div className="products" key={product.id}>
-                    <img src={product.picture} alt="" />
-                    <h2>{product.name}</h2>
-                    <h3>{product.price} $</h3>
-                </div>)}
-
-
             <button className="closeBasket" onClick={() => {
                 value.setOpenbasket('polygon(50% 0, 50% 0, 50% 100%, 50% 100%)');
-                document.body.style.overflow = 'auto'
+                document.body.style.overflow = 'auto';
             }}>X</button>
+            <div className="topBasket">
+                <p>Total : {value.total}$</p>
+                <p>Count : {value.count}</p>
+            </div>
+            <div className="contentBasket">
+                {value.buscetProduct.map(item => <div key={item.id} className="itemBasket">
+                    <img src={item.picture} alt="" />
+                    <h2>{item.name}</h2>
+                    <h3>{item.price}$</h3>
+                    <div className="iconsBasket">
+                        <AiOutlineMinus />
+                        <p>1</p>
+                        <AiOutlinePlus />
+                    </div>
+                    <BsFillTrashFill style={{ color: 'red' }} />
+                </div>)}
+            </div>
         </div>
     )
 }
